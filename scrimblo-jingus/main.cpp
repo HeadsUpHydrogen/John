7d520b110d57742f20cec3ca1703a34b369eebbf
HeadsUpHydrogen <lchaffe02@gmail.com>
2026-06-16 23:44:41 -0400

    Cleanup

--- scrimblo-jingus/main.cpp
+++ scrimblo-jingus/main.cpp
@@ -10,8 +10,7 @@ int main(int argc, char* argv[]) {
 	printf("Universe created with a width of %d, and a height of %d\n", JohnUniverse->GetWidth(), JohnUniverse->GetHeight());
 	Entity* JohnEntity = new Entity(16, 16, 32, JohnUniverse);
 	printf("Entity spawned at %f,%f with a size of %d\n", JohnEntity->GetXPosition(), JohnEntity->GetYPosition(), JohnEntity->GetScale());
-	JohnEntity->SetXPosition(1077.f);
-	JohnEntity->SetYPosition(44.9f);
+	JohnEntity->SetPosition(1077.f, 44.9f);
 	printf("Entity moved to %f, %f\n", JohnEntity->GetXPosition(), JohnEntity->GetYPosition());
 	delete JohnUniverse;
 	delete JohnEntity;
