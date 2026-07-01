2e6471513f279a2df981431f3a0bff5e019c7dc4
HeadsUpHydrogen <lchaffe02@gmail.com>
2026-07-01 15:10:46 -0400

    Big Boss

--- scrimblo-jingus/GetDLL.bat
+++ scrimblo-jingus/GetDLL.bat
@@ -1,1 +1,3 @@
 copy "..\ExternalLibs\JohnBin\bin\John.dll" "..\x64\Debug"
+copy "..\ExternalLibs\SDL\build\SDL3.dll" "..\x64\Debug"
+copy "..\ExternalLibs\SDL_Net\build\SDL3_net.dll" "..\x64\Debug"
